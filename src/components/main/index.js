cec02204d8d790382427728bc04f94a5f9f43034
eridho.rollian <eridho.rollian@e-consult.de>
2020-02-10 15:44:11 +0100

    components main

---
+++ src/components/main/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
+import { constants } from "../../constants";
+
+const Main = ({ value, incrementAction, decreaseAction }) => {
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button onClick={incrementAction}>Increment</button>
+      <button onClick={decreaseAction}>Decrease</button>
+      <ToastContainer autoClose={5000} />
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  value: state.value
+});
+
+const mapDispatchToProps = dispatch => ({
+  incrementAction: () => dispatch(incrementAction()),
+  decreaseAction: () => dispatch(decreaseAction())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
