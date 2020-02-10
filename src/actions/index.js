c6414098c83125a851d0ca1f98ec1aa1edb18624
eridho.rollian <eridho.rollian@e-consult.de>
2020-02-10 15:33:01 +0100

    actions

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import { constants } from "../constants";
+import { toast } from "react-toastify";
+
+export const incrementAction = () => {
+  return function(dispatch) {
+    dispatch({
+      type: constants.INCREMENT
+    });
+    toast.success("Hey its success");
+  };
+};
+
+export const decreaseAction = () => {
+  return function(dispatch) {
+    dispatch({
+      type: constants.DECREASE
+    });
+    toast.warn("Now its decrease");
+  };
+};
